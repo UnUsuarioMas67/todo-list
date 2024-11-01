685adfcf86d447ac29714101868ab5533071fb2d
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2024-10-31 20:36:01 -0400

    Add method for testing

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,15 @@
 import "../style.css";
 import { Task, Project, TodoList } from "./todo.js";
-import { createProjectElement, createDefaultProjectElement } from "./projectsDOM.js";
+import {
+  createProjectElement,
+  createDefaultProjectElement,
+} from "./projectsDOM.js";
 
 window.addEventListener("load", initialize);
 
 function initialize() {
   const todoList = new TodoList();
   todoList.loadFromLocalStorage();
-
-  // const project = new Project("Dummy Project");
-  // project.addTask(
-  //   new Task("Dummy Task", "description", new Date("2025/20/2"), "high")
-  // );
-  // todoList.addProject(project);
-
-  // const project2 = new Project("Dummy Project2");
-  // project.addTask(
-  //   new Task("Dummy Task", "description", new Date("2025/20/2"), "high")
-  // );
-  // todoList.addProject(project2);
 
   renderAllContent(todoList);
   const defaultProject = document.querySelector("#default-project");
@@ -43,4 +34,18 @@ function renderProjectList(todoList) {
 
     projectList.appendChild(projectElem);
   });
+}
+
+function addDummyContent(todoList) {
+  const project = new Project("Dummy Project");
+  project.addTask(
+    new Task("Dummy Task", "description", new Date("2025/20/2"), "high")
+  );
+  todoList.addProject(project);
+
+  const project2 = new Project("Dummy Project2");
+  project2.addTask(
+    new Task("Dummy Task", "description", new Date("2025/20/2"), "high")
+  );
+  todoList.addProject(project2);
 }
